9719e5c192c3ed7b5451e6a9b0d71b76c84abca3
patzwald <harveymoldon@gmail.com>
2024-12-05 13:24:58 +0100

    Changing everything back to 4th Dec

---
+++ menu.js
@@ -0,0 +1,10 @@
+/* Open the sidenav */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "14%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+} 
+
