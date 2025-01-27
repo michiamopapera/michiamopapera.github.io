1092ff3ef5197494e830452f777f99f99b78a33f
patzwald <harveymoldon@gmail.com>
2025-01-27 13:52:37 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,6 +1,6 @@
 /* Open the sidenav */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "14%";
+  document.getElementById("mySidenav").style.width = "28%";
 }
 
 /* Close/hide the sidenav */
