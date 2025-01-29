1523aab3f52823c2089961081ebdcca8308e0a0a
patzwald <harveymoldon@gmail.com>
2025-01-29 15:55:47 +0100

    first official release

---
+++ nav.js
@@ -0,0 +1,10 @@
+/* Open the sidenav */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "170px";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+} 
+
